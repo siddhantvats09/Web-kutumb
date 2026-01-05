3bfc818253a360b5c7dfac297a23033d9590bee3
Siddhant Vats <siddhantvats840@gmail.com>
2026-01-05 13:48:45 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
       <Hero />
       {/* <ServicesSection /> */}
       <Portfolio></Portfolio>
-      
       <WhyUs/>
       <Testimonials></Testimonials>
       <Homes />
